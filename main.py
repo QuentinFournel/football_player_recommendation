e2635f251d3c8fb213a83ff8b6692143b57f52c5
Quentin FOURNEL <55802451+QuentinFournel@users.noreply.github.com>
2024-05-22 11:26:33 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from prétraitement import prétraitement
-from football_data import football_data
-
-if __name__ == '__main__':
-    df = prétraitement()
-    df = football_data(df)
-    df.to_csv('best_players.csv', index=False)
-    print("DataFrame has been downloaded as 'best_players.csv'.")
